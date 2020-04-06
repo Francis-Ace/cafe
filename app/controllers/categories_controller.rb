5c3305c59619ff729778c30cb519a658230964e4
Francis <francisjosecruz@yahoo.ca>
2020-04-06 03:30:14 -0500

    implemented the category index and show action/views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,18 @@
 class CategoriesController < ApplicationController
-  def index
-  end
 
-  def show
+  # Get / triggers this action
+  def index
+    @categories = Category.all
   end
+  # Rails automatically loads the associated view:
+  # app/view/categories/index.html.erb
+  # All @categories var is shared with the view.
+
+  # Get /categories.:id triggers this action
+  def show
+    @category = Category.find(params[:id])
+  end
+  # Rails automatically loads the associated view:
+  # app/view/categories/show.html.erb
+  # All @category var is shared with the view.
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
-  get 'categories/index'
-  get 'categories/show'
+  # get 'products/index'
+  # get 'products/show'
+  # get 'categories/index'
+  # get 'categories/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # GET / => categories#index
+  root to: 'categories#index'
+
+  # GET /categories/:id => categories/show, will be know as category_path
+  get 'categories/:id', to: 'categories#show', id: /\d+/, as: 'category'
 end
