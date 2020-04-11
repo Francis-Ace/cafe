4a6c64f38ccfdd8ab6630aebf12c6421cfbfb270
Francis <francisjosecruz@yahoo.ca>
2020-04-10 20:45:35 -0500

    using the unsplash api attach image to each product

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,7 +44,7 @@ beer = Category.create(
 products_per_categories = rand(25..30)
 # Creates the 10 products under the coffee category
 products_per_categories.times do
-  product_name = Faker::Coffee.blend_name
+  product_name = Faker::Coffee.unique.blend_name
   product_description = Faker::Coffee.notes
   product_price = rand(5.5..15.9)
   product_quantity = rand(20..50)
@@ -52,12 +52,16 @@ products_per_categories.times do
                          price:       product_price,
                          description: product_description,
                          quantity:    product_quantity)
+
+  downloaded_image = open(URI.escape("https://source.unsplash.com/600x600/?#{[product.name]}"))
+  product.image.attach(io: downloaded_image, filename: "m-#{[product.name, product_quantity].join('-')}.jpg")
+  sleep(1) # Throttle a tad. Needed?
 end
 
 products_per_categories = rand(25..30)
 # Creates the 10 products under the food category
 products_per_categories.times do
-  product_name2 = Faker::Food.dish
+  product_name2 = Faker::Food.unique.dish
   product_description2 = Faker::Food.description
   product_price2 = rand(5.5..15.9)
   product_quantity2 = rand(20..50)
@@ -65,12 +69,16 @@ products_per_categories.times do
                        price:       product_price2,
                        description: product_description2,
                        quantity:    product_quantity2)
+
+  downloaded_image = open(URI.escape("https://source.unsplash.com/600x600/?#{[product2.name]}"))
+  product2.image.attach(io: downloaded_image, filename: "m-#{[product2.name, product_quantity2].join('-')}.jpg")
+  sleep(1) # Throttle a tad. Needed?
 end
 
-products_per_categories = rand(25..30)
+products_per_categories = rand(15..20)
 # Creates the 10 products under the dessert category
 products_per_categories.times do
-  product_name3 = Faker::Dessert.variety
+  product_name3 = Faker::Dessert.unique.variety
   product_description3 = Faker::Dessert.flavor
   product_price3 = rand(5.5..15.9)
   product_quantity3 = rand(20..50)
@@ -78,22 +86,32 @@ products_per_categories.times do
                           price:       product_price3,
                           description: product_description3,
                           quantity:    product_quantity3)
+
+  downloaded_image = open(URI.escape("https://source.unsplash.com/600x600/?#{[product3.name]}"))
+  product3.image.attach(io: downloaded_image, filename: "m-#{[product3.name, product_quantity3].join('-')}.jpg")
+  sleep(1) # Throttle a tad. Needed?
 end
 
 products_per_categories = rand(25..30)
 # Creates the 10 products under the dessert category
 products_per_categories.times do
-  product_name4 = Faker::Beer.name
+  product_name4 = Faker::Beer.unique.name
   product_description4 = Faker::Beer.style
   product_price4 = rand(5.5..15.9)
   product_quantity4 = rand(20..50)
-  beer.products.create(name:        product_name4,
-                       price:       product_price4,
-                       description: product_description4,
-                       quantity:    product_quantity4)
+  product4 = beer.products.create(name:        product_name4,
+                                  price:       product_price4,
+                                  description: product_description4,
+                                  quantity:    product_quantity4)
+
+  downloaded_image = open(URI.escape("https://source.unsplash.com/600x600/?#{[product4.name]}"))
+  product4.image.attach(io: downloaded_image, filename: "m-#{[product4.name, product_quantity4].join('-')}.jpg")
+  sleep(1) # Throttle a tad. Needed?
 end
 
 puts "Created #{Category.count} Categories"
 puts "Created #{Product.count} Products"
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+if Rails.env.development?
+  AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password")
+end
